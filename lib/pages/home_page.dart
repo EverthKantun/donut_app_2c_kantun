9c22121790455ed1aeb25ede37d233ddbc1ea5bb
EverthKantun <everth.ko@gmail.com>
2025-02-12 13:22:32 -0600

    Main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,11 +12,46 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-      backgroundColor: Colors.transparent,
-      // ícono izquierdo
-      leading: Icon(Icons.menu, color: Colors.grey[800]),
-      //Ícono derecho
-      actions: [Icon(Icons.person)],
-    ));
+          backgroundColor: Colors.transparent,
+          // ícono izquierdo
+          leading: Icon(Icons.menu, color: Colors.grey[800]),
+          //Ícono derecho
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 24.0),
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            //texto principal
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+              child: Row(
+                children: [
+                  Text(
+                    "I want to ",
+                    style: TextStyle(fontSize: 32),
+                  ),
+                  Text("Eat",
+                      style: TextStyle(
+                          //tamaño de letra
+                          fontSize: 32,
+                          //negritas
+                          fontWeight: FontWeight.bold,
+                          //subrayado
+                          decoration: TextDecoration.underline))
+                ],
+              ),
+            ),
+
+            //pestañas o TabBar
+
+            // contenido de pestañas (TabBarView)
+
+            //Carrito (cart)
+          ],
+        ));
   }
 }
