c2e341491d99d52c3254cae659662f7d64fa200a
EverthKantun <everth.ko@gmail.com>
2025-02-14 13:33:03 -0600

    Starting TabBarView

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,9 @@
+import 'package:donut_app_2c_kantun/tabs/burger_tab.dart';
+import 'package:donut_app_2c_kantun/tabs/donut_tab.dart';
+import 'package:donut_app_2c_kantun/tabs/pancakes_tab.dart';
+import 'package:donut_app_2c_kantun/tabs/pizza_dart.dart';
+import 'package:donut_app_2c_kantun/tabs/smoothie_tab.dart';
+import 'package:donut_app_2c_kantun/utils/my_tab.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,50 +14,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Widget> myTabs = [
+    const MyTab(iconPath: 'lib/icons/donut.png'),
+    const MyTab(iconPath: 'lib/icons/burger.png'),
+    const MyTab(iconPath: 'lib/icons/smoothie.png'),
+    const MyTab(iconPath: 'lib/icons/pancakes.png'),
+    const MyTab(iconPath: 'lib/icons/pizza.png')
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          // ícono izquierdo
-          leading: Icon(Icons.menu, color: Colors.grey[800]),
-          //Ícono derecho
-          actions: const [
-            Padding(
-              padding: EdgeInsets.only(right: 24.0),
-              child: Icon(Icons.person),
-            )
-          ],
-        ),
-        body: Column(
-          children: [
-            //texto principal
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
-              child: Row(
-                children: [
-                  Text(
-                    "I want to ",
-                    style: TextStyle(fontSize: 32),
-                  ),
-                  Text("Eat",
-                      style: TextStyle(
-                          //tamaño de letra
-                          fontSize: 32,
-                          //negritas
-                          fontWeight: FontWeight.bold,
-                          //subrayado
-                          decoration: TextDecoration.underline))
-                ],
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            // ícono izquierdo
+            leading: Icon(Icons.menu, color: Colors.grey[800]),
+            //Ícono derecho
+            actions: const [
+              Padding(
+                padding: EdgeInsets.only(right: 24.0),
+                child: Icon(Icons.person),
+              )
+            ],
+          ),
+          body: Column(
+            children: [
+              //texto principal
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+                child: Row(
+                  children: [
+                    Text(
+                      "I want to ",
+                      style: TextStyle(fontSize: 32),
+                    ),
+                    Text("Eat",
+                        style: TextStyle(
+                            //tamaño de letra
+                            fontSize: 32,
+                            //negritas
+                            fontWeight: FontWeight.bold,
+                            //subrayado
+                            decoration: TextDecoration.underline))
+                  ],
+                ),
               ),
-            ),
 
-            //pestañas o TabBar
-
-            // contenido de pestañas (TabBarView)
-
-            //Carrito (cart)
-          ],
-        ));
+              //pestañas o TabBar
+              TabBar(tabs: myTabs),
+              // contenido de pestañas (TabBarView)
+              Expanded(
+                child: TabBarView(children: [
+                  DonutTab(),
+                  BurgerTab(),
+                  SmoothiesTab(),
+                  PanCakesTab(),
+                  PizzaTab()
+                ]),
+              )
+              //Carrito (cart)
+            ],
+          )),
+    );
   }
 }
